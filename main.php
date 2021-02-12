8a518a82a9938b8a26d9d795f7d573285361cdb1
kburgosdavila <71047387+kburgosdavila@users.noreply.github.com>
2021-02-11 20:15:35 -0500

    Create main.php

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+
+$names = array('Chris','Samuel','Luisa')
+
+foreach ($names as $value) {
+  echo "$value <br>";
+}
+?>
+
+<?php
+$zipcode = array("East Campus"=>"32825", "Downtown Campus"=>"32801", "Winter Park Campus"=>"32789");
+
+foreach ($zipcode as $value) {
+    echo "$value <br>";
+  }
+?>
